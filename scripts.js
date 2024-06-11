edf5a9fa43548b0fb38e6da21c807981793c01a4
Lucid-Ninja-noob <lucid.mikey@gmail.com>
2024-06-11 12:46:37 +0200

    Delete scripts.js

--- scripts.js
+++
@@ -1,8 +0,0 @@
-function readMore(postId) {
-    var post = document.getElementById(postId);
-    if (post.style.display === "block") {
-        post.style.display = "none";
-    } else {
-        post.style.display = "block";
-    }
-}
